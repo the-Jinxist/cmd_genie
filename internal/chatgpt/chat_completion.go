3b355a8ed471233b1b0c97e4d9bd0c34ab69a4f5
Favour Olukayode <nerosilva522@gmail.com>
2024-10-17 18:29:07 +0100

    feat: added client code

---
+++ internal/chatgpt/chat_completion.go
@@ -0,0 +1,76 @@
+package chatgpt
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type ChatCompletion struct {
+	client *Client
+}
+
+// newSubscription returns a new Subscription.
+func newChatCompletion(client *Client) *ChatCompletion {
+	return &ChatCompletion{
+		client: client,
+	}
+}
+
+type GeminiRequest struct {
+	Contents []Contents `json:"contents"`
+}
+type Parts struct {
+	Text string `json:"text"`
+}
+type Contents struct {
+	Parts []Parts `json:"parts"`
+}
+
+type GeminiResponse struct {
+	Candidates    []Candidates  `json:"candidates"`
+	UsageMetadata UsageMetadata `json:"usageMetadata"`
+}
+
+type Content struct {
+	Parts []Parts `json:"parts"`
+	Role  string  `json:"role"`
+}
+type SafetyRatings struct {
+	Category    string `json:"category"`
+	Probability string `json:"probability"`
+}
+type Candidates struct {
+	Content       Content         `json:"content"`
+	FinishReason  string          `json:"finishReason"`
+	Index         int             `json:"index"`
+	SafetyRatings []SafetyRatings `json:"safetyRatings"`
+}
+type UsageMetadata struct {
+	PromptTokenCount     int `json:"promptTokenCount"`
+	CandidatesTokenCount int `json:"candidatesTokenCount"`
+	TotalTokenCount      int `json:"totalTokenCount"`
+}
+
+func (c ChatCompletion) getChatCompletion(prompt string) (*GeminiResponse, error) {
+
+	//TODO: CHECK IF YOU CAN DETECT THE KIND OF OS IS IT
+	url := fmt.Sprintf("v1beta/models/gemini-1.5-flash-latest:generateContent?key=%s", c.client.APIKey)
+	body := GeminiRequest{
+		Contents: []Contents{
+			{
+				Parts: []Parts{
+					{
+						Text: prompt,
+					},
+					{
+						Text: "You're a CLI savant with experience in naviagting CLIs on all operating system. Please provide succintly the command related to what the prompt needs",
+					},
+				},
+			},
+		},
+	}
+	res := new(GeminiResponse)
+	err := c.client.req(http.MethodPost, url, body, res)
+
+	return res, err
+}
---
+++ internal/chatgpt/chatgpt.go
@@ -0,0 +1,72 @@
+package chatgpt
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+type Client struct {
+	APIKey     string
+	HttpClient *http.Client
+	log        *slog.Logger
+	BaseUrl    *url.URL
+
+	ChatCompletion *ChatCompletion
+
+	// Transaction      *Transaction
+	// TransactionSplit *TransactionSplit
+	// Plan             *Plans
+	// Subscription     *Subscription
+}
+
+const BASE_URL = "https://api.openai.com/"
+
+func NewClient(apiKey string) *Client {
+	httpClient := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	parsedUrl, _ := url.Parse(BASE_URL)
+	c := &Client{APIKey: apiKey, HttpClient: httpClient, log: logger, BaseUrl: parsedUrl}
+	c.ChatCompletion = newChatCompletion(c)
+	return c
+}
+
+func (c Client) req(method string, url string, body interface{}, response interface{}) error {
+
+	var reqBody *bytes.Buffer
+	if body != nil {
+		buf, err := json.Marshal(body)
+		if err != nil {
+			return err
+		}
+
+		reqBody = bytes.NewBuffer(buf)
+	}
+
+	reqUrl, _ := c.BaseUrl.Parse(url)
+
+	req, _ := http.NewRequest(method, reqUrl.String(), reqBody)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.APIKey))
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error processing request - %+v", err)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return fmt.Errorf("error while unmarshalling the response bytes %+v ", err)
+	}
+
+	return nil
+}

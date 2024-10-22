287410e806da2e5429c5ea0024cd60c4c8648ba7
Favour Olukayode <nerosilva522@gmail.com>
2024-10-22 14:21:20 +0100

    chore: update releaser

--- config/env.go
+++ config/env.go
@@ -41,7 +41,7 @@ func LoadConfigs(path string) (Config, error) {
 
 	viper.AutomaticEnv()
 
-	if viper.GetString(EnvKey) == "dev" {
+	if viper.GetString(EnvKey) != "prod" {
 
 		err = viper.ReadInConfig()
 		if err != nil {
